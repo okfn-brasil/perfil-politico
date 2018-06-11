7292bf490e5ba927584fc7efd021094c5e00a071
Leticia Portella <leportella@gmail.com>
2018-06-11 11:14:38 -0300

    added search by state

--- api/candidates/views.py
+++ api/candidates/views.py
@@ -7,10 +7,19 @@ from restless.serializers import JSONSerializer
 from .models import Candidates
 
 
-class PersonResource(DjangoResource):
+class CandidateResource(DjangoResource):
     serializer = JSONSerializer()
 
     def detail(self, pk):
         data = json.loads(Candidates.objects.get(cpf=pk).to_json())
         data.pop('_id')
+        return data
+
+
+class StateResource(DjangoResource):
+    serializer = JSONSerializer()
+
+    def detail(self, pk):
+        candidates = Candidates.objects.filter(state=pk)
+        data = [json.loads(c.to_json()) for c in candidates]
         return data
