efe4be7e5cd0b887f63b91514d9eb9ef8bb0e264
Leticia Portella <leportella@gmail.com>
2018-06-29 15:43:20 -0300

    add utils folder with some helpers

---
+++ api/utils/translation.py
@@ -0,0 +1,34 @@
+GENDERS = {'MASCULINO': 'male', 'FEMININO': 'female'}
+
+POSITIONS = {
+    'VEREADOR': 'CITY COUNCILMAN',
+    'PREFEITO - INTERINO': 'MAYOR - INTERIM',
+    'PREFEITO': 'MAYOR',
+    'VICE-PREFEITO': 'VICE-MAYOR',
+    'GOVERNADOR': 'GOVERNOR',
+    'VICE-GOVERNADOR': 'VICE-GOVERNOR',
+    'DEPUTADO FEDERAL': 'FEDERAL DEPUTY',
+    'DEPUTADO ESTADUAL': 'STATE DEPUTY',
+    'SENADOR': 'SENATE',
+    'PRESIDENTE': 'PRESIDENT',
+    'VICE-PRESIDENTE': 'VICE-PRESIDENT',
+}
+
+
+ELECTION_RESULT = {
+    'SUPLENTE': 'alternate',
+    'NÃO ELEITO': 'not_elected',
+    'ELEITO POR QP': 'elected_by_party_quota',
+    'ELEITO': 'elected',
+    'ELEITO POR MÉDIA': 'elected',
+    'MÉDIA': 'elected',
+    'REGISTRO NEGADO ANTES DA ELEIÇÃO': 'rejected',
+    'INDEFERIDO COM RECURSO': 'rejected',
+    'RENÚNCIA/FALECIMENTO/CASSAÇÃO ANTES DA ELEIÇÃO': 'rejected',
+    '2º TURNO': 'runoff',
+    'SUBSTITUÍDO': 'replaced',
+    'REGISTRO NEGADO APÓS A ELEIÇÃO': 'rejected',
+    'RENÚNCIA/FALECIMENTO COM SUBSTITUIÇÃO': 'replaced',
+    'RENÚNCIA/FALECIMENTO/CASSAÇÃO APÓS A ELEIÇÃO': 'rejected',
+    'CASSADO COM RECURSO': 'rejected',
+}
