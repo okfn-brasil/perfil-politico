ae9fe5d445ce802dfa53bb4f46931324f32f3e7b
Leticia Portella <leportella@gmail.com>
2018-07-20 16:08:07 -0300

    add admin ordering on election and party apps

--- perfil/election/admin.py
+++ perfil/election/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Election
 
 
-admin.site.register(Election)
+@admin.register(Election)
+class ElectionAdmin(admin.ModelAdmin):
+    ordering = ('-year',)
--- perfil/party/admin.py
+++ perfil/party/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Party
 
 
-admin.site.register(Party)
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    ordering = ('initials',)
