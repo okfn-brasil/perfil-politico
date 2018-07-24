91adbb1423d846886e312514b74c9dbbbe7630c5
Leticia Portella <leportella@gmail.com>
2018-07-24 14:39:21 -0300

    update models and migrations

--- perfil/election/models.py
+++ perfil/election/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 from party.models import Party
 from person.models import Person
+from .choices import ASSETS, ELECTION_RESULT, POSITIONS
 
 
 class Election(models.Model):
-    year = models.IntegerField()
-    party = models.ForeignKey(Party, on_delete=models.CASCADE)
-    position = models.CharField(max_length=250)
-    legend_name = models.CharField(max_length=250)
-    legend_composition = models.CharField(max_length=250)
-    result = models.CharField(max_length=250)
     candidate = models.ForeignKey(Person, on_delete=models.CASCADE)
+    candidate_sequential = models.CharField(max_length=50)
+    legend_composition = models.CharField(max_length=250)
+    legend_name = models.CharField(max_length=250)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
     place = models.CharField(max_length=250)
+    position = models.CharField(max_length=2, choices=POSITIONS)
+    result = models.CharField(max_length=1, choices=ELECTION_RESULT)
     state = models.CharField(max_length=250)
+    year = models.IntegerField()
 
     def __str__(self):
         return '{} - {} - {}'.format(self.candidate.civil_name,
--- perfil/person/models.py
+++ perfil/person/models.py
@@ -5,14 +5,15 @@ from utils.infos import STATES
 
 class Person(models.Model):
     civil_name = models.CharField(max_length=250)
-    cpf = models.CharField(max_length=14)
-    gender = models.CharField(max_length=250)
-    voter_id = models.CharField(max_length=250)
-    birthday = models.CharField(max_length=250)
+    cpf = models.CharField(max_length=14, unique=True)
+    gender = models.CharField(max_length=250, blank=True)
+    voter_id = models.CharField(max_length=250, blank=True)
+    birthday = models.CharField(max_length=250, blank=True)
     birthdate = models.DateField(max_length=250, null=True)
-    birthplace_city = models.CharField(max_length=250)
-    birthplace_state = models.CharField(max_length=2, choices=STATES)
-    occupation = models.CharField(max_length=250)
+    birthplace_city = models.CharField(max_length=250, blank=True)
+    birthplace_state = models.CharField(max_length=2, choices=STATES,
+                                        blank=True)
+    occupation = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return self.civil_name
