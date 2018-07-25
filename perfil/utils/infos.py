23c3abd36d6ca63a91d8f3972c4eefc1237321ac
Eduardo Cuducos <cuducos@gmail.com>
2018-07-25 12:58:14 -0300

    Refactor TERMS_BY_YEAR

--- perfil/utils/infos.py
+++ perfil/utils/infos.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 STATES = (
     ('AC', 'Acre'),
     ('AL', 'Alagoas'),
@@ -29,83 +32,16 @@ STATES = (
     ('ZZ', 'Exterior'),
 )
 
-
-TERMS_BY_NUMBER = {
-    55: {
-        'begin': 2015,
-        'end': 2019,
-    },
-    54: {
-        'begin': 2011,
-        'end': 2015,
-    },
-    53: {
-        'begin': 2007,
-        'end': 2011,
-    },
-    52: {
-        'begin': 2003,
-        'end': 2007,
-    },
-    51: {
-        'begin': 1999,
-        'end': 2003,
-    },
-    50: {
-        'begin': 1995,
-        'end': 1999,
-    },
-    49: {
-        'begin': 1991,
-        'end': 1995,
-    },
-    48: {
-        'begin': 1987,
-        'end': 1991,
-    },
-}
-
+Term = namedtuple('Term', ('begins', 'ends', 'number'))
 TERMS_BY_YEAR = {
-    2015: {
-        'begin': 2015,
-        'end': 2019,
-        'number': 55,
-    },
-    2011: {
-        'begin': 2011,
-        'end': 2015,
-        'number': 54,
-    },
-    2007: {
-        'begin': 2007,
-        'end': 2011,
-        'number': 53,
-    },
-    2003: {
-        'begin': 2003,
-        'end': 2007,
-        'number': 52,
-    },
-    1999: {
-        'begin': 1999,
-        'end': 2003,
-        'number': 51,
-    },
-    1995: {
-        'begin': 1995,
-        'end': 1999,
-        'number': 50,
-    },
-    1991: {
-        'begin': 1991,
-        'end': 1995,
-        'number': 49,
-    },
-    1987: {
-        'begin': 1987,
-        'end': 1991,
-        'number': 48,
-    },
+    2015: Term(2015, 2019, 55),
+    2011: Term(2011, 2015, 54),
+    2007: Term(2007, 2011, 53),
+    2003: Term(2003, 2007, 52),
+    1999: Term(1999, 2003, 51),
+    1995: Term(1995, 1999, 50),
+    1991: Term(1991, 1995, 49),
+    1987: Term(1987, 1991, 48),
 }
 
 POSITIONS = {
