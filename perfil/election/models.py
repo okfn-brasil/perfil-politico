cedf21e8a0c24884bff918303e4e0a786ebaa262
Eduardo Cuducos <cuducos@gmail.com>
2018-08-01 11:39:16 -0300

    Add import command for donations

--- perfil/election/models.py
+++ perfil/election/models.py
@@ -18,6 +18,21 @@ class Election(models.Model):
     year = models.IntegerField()
 
     def __str__(self):
-        return '{} - {} - {}'.format(self.candidate.civil_name,
-                                     self.year,
-                                     self.position)
+        return f'{self.candidate.civil_name} - {self.year} - {self.position}'
+
+
+class Donation:
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    donator = models.CharField(max_length=250)
+    donator_id = models.CharField(max_length=14)
+    original_donator = models.CharField(max_length=250, null=True)
+    original_donator_id = models.CharField(max_length=14, null=True)
+    date = models.DateField()
+    value = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=250)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['donator_id']),
+            models.Index(fields=['value'])
+        ]
