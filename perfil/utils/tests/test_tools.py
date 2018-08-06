32f36ab68e6d6a0990fd6d0ecfabe4dab9cd87bf
Eduardo Cuducos <cuducos@gmail.com>
2018-08-06 20:35:44 -0300

    Add import command for donations

--- perfil/utils/tests/test_tools.py
+++ perfil/utils/tests/test_tools.py
@@ -1,19 +1,50 @@
 from datetime import date
+from decimal import Decimal
 
 import pytest
 
-from perfil.utils.tools import normalize, parse_birthdate, probably_same_entity
+from perfil.utils.tools import (
+    normalize,
+    parse_date,
+    parse_decimal,
+    parse_document,
+    probably_same_entity
+)
 
 
-@pytest.mark.parametrize('input,expected', [
+@pytest.mark.parametrize('input, expected', [
     ('1/1/1979', date(1979, 1, 1)),
     ('1/1/79', date(1979, 1, 1)),
     (None, None),
     ('', None),
-    ('1/1', None),
+    ('1/1', None)
 ])
-def test_parse_birthdate(input, expected):
-    assert parse_birthdate(input) == expected
+def test_parse_date(input, expected):
+    assert parse_date(input) == expected
+
+
+@pytest.mark.parametrize('input, expected', [
+    ('342.663.749-92', '34266374992'),
+    ('34266374992', '34266374992'),
+    ('000.000.000-00', None),
+    ('51.771.685/0001-00', '51771685000100'),
+    ('51771685000100', '51771685000100'),
+    ('00.000.000/0000-00', None),
+    (None, None),
+    ('42', None),
+    ('1/1', None)
+])
+def test_parse_document(input, expected):
+    assert parse_document(input) == expected
+
+
+@pytest.mark.parametrize('input, expected', [
+    ('3.1415', Decimal('3.1415')),
+    ('abc', None),
+    (None, None)
+])
+def test_parse_decimal(input, expected):
+    assert parse_decimal(input) == expected
 
 
 def test_normalize():
--- perfil/utils/tools.py
+++ perfil/utils/tools.py
@@ -1,7 +1,11 @@
+import re
 import unicodedata
 from datetime import datetime
+from decimal import Decimal, InvalidOperation
 from itertools import combinations
 
+from brazilnum.cnpj import validate_cnpj
+from brazilnum.cpf import validate_cpf
 from Levenshtein import distance
 
 
@@ -28,18 +32,36 @@ MONTHS = {
 }
 
 
-def parse_birthdate(birthday):
-    if not birthday or not isinstance(birthday, str):
+def parse_date(date):
+    if not date or not isinstance(date, str):
         return
 
     patterns = ('%d/%m/%Y', '%d/%m/%y')
     for pattern in patterns:
         try:
-            return datetime.strptime(birthday, pattern).date()
-        except ValueError:
+            return datetime.strptime(date, pattern).date()
+        except (ValueError, TypeError):
             pass
 
     return None
+
+
+def parse_decimal(value, ignore_errors=True):
+    try:
+        return Decimal(value)
+    except (InvalidOperation, TypeError):
+        return None
+
+
+def parse_document(value):
+    if not isinstance(value, (str, bytes)):
+        return None
+
+    cleaned = re.sub(r'\D', '', value)
+    if not validate_cnpj(cleaned) and not validate_cpf(cleaned):
+        return None
+
+    return cleaned
 
 
 def treat_birthday(date):
