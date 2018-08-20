67f49afd6d9de56342fa4225ce292f016ff60494
Leticia Portella <leportella@gmail.com>
2018-08-20 13:39:34 -0300

    improve detauls on load_person_info

--- perfil/person/management/commands/load_person_info.py
+++ perfil/person/management/commands/load_person_info.py
@@ -1,5 +1,8 @@
+from textwrap import dedent
+
 from django.core.management.base import BaseCommand
 
+from rows.plugins.utils import ipartition
 from tqdm import tqdm
 
 from perfil.person.models import Person, PersonInformation
@@ -7,7 +10,10 @@ from perfil.person.models import Person, PersonInformation
 
 class Command(BaseCommand):
 
-    help = ""
+    help = dedent(""""
+        Fill the auxiliary table PersonInformation based on models
+        Person, PartyFiliation, Election, Asset
+    """)
     model = PersonInformation
     bulk_size = 2 ** 10
     slice_csv = False
@@ -17,39 +23,51 @@ class Command(BaseCommand):
         people = Person.objects.all()
         total = people.count()
 
+        # choices of Federal Deputy and Senate on Election model
         position_congress = ['15', '16', '17', '18', '25', '26', '27', '28']
+
+        # choices of Elected and Elected by Party Quota on Election model
+        elected_choices = ['1', '2']
 
         with tqdm(total=total, desc=desc, unit='people') as progress:
             infos = []
-            count = 0
-            for p in people:
-                election_parties = p.election_parties
-                filiation_parties = p.filiation_parties
+            for person in people:
+                election_parties = person.election_parties
+                filiation_parties = person.filiation_parties
                 parties_change = len(set(filiation_parties + election_parties))
 
-                elections_results = list(p.elections.values_list('result',
-                                                                 flat=True))
+                elections_results = list(
+                    person.elections.values_list('result', flat=True)
+                )
                 num_elections = len(elections_results)
                 elected_by_quota = elections_results.count('2')
                 elected = elections_results.count('1')
                 total_elections_won = elected_by_quota + elected
 
                 congressperson = False
-                elections = list(p.elections.values_list('position', 'result'))
+                elections = list(person.elections.values_list('position',
+                                                              'result'))
+
+                # Check if person was elected to federal deputy or senate
                 for election in elections:
                     position = election[0]
                     result = election[1]
-                    if position in position_congress and result in ['1', '2']:
+
+                    verification = [
+                        position in position_congress,
+                        result in elected_choices,
+                    ]
+                    if all(verification):
                         congressperson = True
                         break
 
                 infos.append(PersonInformation(
-                    person=p,
-                    civil_name=p.civil_name,
+                    person=person,
+                    civil_name=person.civil_name,
                     num_elections=num_elections,
                     total_elections_won=total_elections_won,
                     num_elections_won_by_quota=elected_by_quota,
-                    biggest_asset_evolutions=p.biggest_asset_evolution,
+                    biggest_asset_evolutions=person.biggest_asset_evolution,
                     election_parties=str(election_parties),
                     election_parties_changed=len(election_parties),
                     filiation_parties=set(filiation_parties),
@@ -57,8 +75,7 @@ class Command(BaseCommand):
                     total_parties_changed=parties_change,
                     elected_as_congressperson=congressperson,
                 ))
-                count += 1
-                if count == self.bulk_size:
-                    PersonInformation.objects.bulk_create(infos)
-                    infos, count = [], 0
-                    progress.update(self.bulk_size)
+
+            for bulk in ipartition(infos, self.bulk_size):
+                PersonInformation.objects.bulk_create(bulk)
+                progress.update(len(bulk))
